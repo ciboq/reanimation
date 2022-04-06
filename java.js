9c31f32146c2bbc6b9cb574fd4b75057ca1dd730
ciboq <90256806+ciboq@users.noreply.github.com>
2022-04-06 02:24:05 -0400

    m

--- java.js
+++ java.js
@@ -1,8 +1,10 @@
-let screenLog = document.querySelector('#screen-log');
-document.addEventListener('mousemove', logKey);
-
-function logKey(e) {
-  screenLog.innerHTML = `
-    Screen X/Y: ${e.screenX}, ${e.screenY}
-    Client X/Y: ${e.clientX}, ${e.clientY}`;
-}
+$(document).ready(function(){
+  var currentMousePos = { x: -1, y: -1 };
+  $('#mask1').mousemove(function(e) {
+      currentMousePos.x = e.pageX;
+      currentMousePos.y = e.pageY;
+    
+    $('#mask2').css('-webkit-mask-position-x', currentMousePos.x - 75);
+    $('#mask2').css('-webkit-mask-position-y', currentMousePos.y - 75)
+  });
+});
